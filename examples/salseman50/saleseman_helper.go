b996ad2f89eb97ed7c1de2535c8315c182e439bd
matsulib <tmy.matsu@gmail.com>
2017-10-01 00:06:50 +0900

    Delete unused code

--- examples/salseman50/saleseman_helper.go
+++ examples/salseman50/saleseman_helper.go
@@ -91,26 +91,7 @@ func NewAmerica() *America {
 		"Cable Car Museum, San Francisco, CA":   {37.794781, 122.411715},
 		"San Andreas Fault, CA":                 {36.576088, 120.987632},
 		"Hoover Dam, NV":                        {36.016066, 114.737732}}
-	/*"New York City": {40.72, 74.00},
-	"Los Angeles":   {34.05, 118.25},
-	"Chicago":       {41.88, 87.63},
-	"Houston":       {29.77, 95.38},
-	"Phoenix":       {33.45, 112.07},
-	"Philadelphia":  {39.95, 75.17},
-	"San Antonio":   {29.53, 98.47},
-	"Dallas":        {32.78, 96.80},
-	"San Diego":     {32.78, 117.15},
-	"San Jose":      {37.30, 121.87},
-	"Detroit":       {42.33, 83.05},
-	"San Francisco": {37.78, 122.42},
-	"Jacksonville":  {30.32, 81.70},
-	"Indianapolis":  {39.78, 86.15},
-	"Austin":        {30.27, 97.77},
-	"Columbus":      {39.98, 82.98},
-	"Fort Worth":    {32.75, 97.33},
-	"Charlotte":     {35.23, 80.85},
-	"Memphis":       {35.12, 89.97},
-	"Baltimore":     {39.28, 76.62}}*/
+
 	// create a distance matrix
 	a.DistanceMatrix = map[string]DMap{}
 	for ka, va := range a.Cities {
